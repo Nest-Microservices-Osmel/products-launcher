3553c2f4db4e8675cd205e4dfaf697e3059c3a6a
Osmel <osmel.santos.88@gmail.com>
2024-08-23 16:40:20 -0400

    order

--- orders-ms/src/orders/orders.controller.ts
+++ orders-ms/src/orders/orders.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Logger, ParseUUIDPipe } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderPaginationDto } from './dto/order-pagination.dto';
-import { ChangeOrderStatusDto } from './dto';
+import { ChangeOrderStatusDto, PaidOrderDto } from './dto';
+import { OrderWithProducts } from './interfaces/order-with-products.interface';
 
 
 @Controller()
@@ -12,8 +13,13 @@ export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @MessagePattern('createOrder')
-  create(@Payload() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+  async create(@Payload() createOrderDto: CreateOrderDto) {
+    const order = await this.ordersService.create(createOrderDto);
+    const paymentSession = await this.ordersService.createPaymentSession(order)
+    return {
+      order,
+      paymentSession
+    };
   }
 
   @MessagePattern('findAllOrders')
@@ -29,6 +35,13 @@ export class OrdersController {
   @MessagePattern('changeOrderStatus')
   changeOrderStatus(@Payload() changeOrderStatusDto: ChangeOrderStatusDto) {
     return this.ordersService.chanceStatus(changeOrderStatusDto)
+  }
+
+
+  @EventPattern('payment.succeeded')
+  payOrder(@Payload() paidOrderDto: PaidOrderDto){
+    console.log({ paidOrderDto });
+    return this.ordersService.paidOrder(paidOrderDto)
   }
 
 }
